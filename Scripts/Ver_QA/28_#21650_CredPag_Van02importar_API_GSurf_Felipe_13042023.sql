1a9200d54ecaf08edb7969b65885a2a1ff9f08bf
thilipe <felipe.podipa@gmail.com>
2023-04-13 16:40:04 -0300

    APIGsurf

---
+++ Scripts/Ver_QA/28_#21650_CredPag_Van02importar_API_GSurf_Felipe_13042023.sql
@@ -0,0 +1,9 @@
+
+insert into van07 
+(UNG2Cod,VanCod,VanUngWbsUrl,VanUngWbsPor,VanUngWbsTimOut,VanUngWbsUsr,VanUngWbsPwd,VanUngBasUrl,VanUngTkn,VanUngPcbEstCod,VanUngPcbStfIde)
+values(24,8,'api.gsurfnet.com',443,120,'6090300e-2c9f-4b5a-92d0-a15c626f72ec','pR6A3vAIPoymlfDiIJsFMxfRgsE2JpHK','/transactions-v2/','NjA5MDMwMGUtMmM5Zi00YjVhLTkyZDAtYTE1YzYyNmY3MmVjOnBSNkEzdkFJUG95bWxmRGlJSnNGTXhmUmdzRTJKcEhL',56,'00000064')
+
+insert into PARSIS
+(parcod,ParDsc,ParTipPar,ParTamPar,ParCon)
+values
+('API_AUT_GSURF','Gerar autenticação token','CA',300,'/gmac-v1/oauth2/')
