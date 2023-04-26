ed1bdc890dce1caf330720f8ebef6ca55e66bb9c
lvsantosdev <ldsantos.developer@gmail.com>
2023-04-26 10:48:24 -0300

    script telas parametros e tabelas conciliacao pix

--- Scripts/Ver_QA/23_#21609_Tabela_trn13_parametros_Leonardo_12042023.sql
+++ Scripts/Ver_QA/23_#21609_Tabela_trn13_parametros_Leonardo_12042023.sql
@@ -26,6 +26,25 @@ INSERT INTO sse2_grp_mod
 VALUES (25, 'ADM', 'TransacoesPix_IMP') --Verificar a unidade de negócio para modificar de '25' se for diferente da Pronto
 
 
+CREATE TABLE [StatusConciliacao] (
+  [StatusConciliacaoId]   INT    NOT NULL,
+  [StatusConciliacaoDesc] VARCHAR(100)    NOT NULL,
+     PRIMARY KEY ( [StatusConciliacaoId] ))
+
+	 
+CREATE TABLE [PixCon] (
+  [PixConId]            DECIMAL(18)    NOT NULL    IDENTITY ( 1 , 1 ),
+  [PixConNomeArq]       VARCHAR(150)    NOT NULL,
+  [PixConDataInc]       DATETIME    NOT NULL,
+  [PixConDataCon]       DATETIME    NULL,
+  [StatusConciliacaoId] INT    NOT NULL,
+     PRIMARY KEY ( [PixConId] ))
+CREATE NONCLUSTERED INDEX [IPIXCON1] ON [PixCon] (
+      [StatusConciliacaoId])
+ALTER TABLE [PixCon]
+ ADD CONSTRAINT [IPIXCON1] FOREIGN KEY ( [StatusConciliacaoId] ) REFERENCES [StatusConciliacao]([StatusConciliacaoId])
+
+ 
 CREATE TABLE [Trn13] (
   [Trn13Id]             DECIMAL(18)    NOT NULL    IDENTITY ( 1 , 1 ),
   [Trn13NomeArq]        VARCHAR(100)    NOT NULL,
@@ -38,4 +57,16 @@ CREATE TABLE [Trn13] (
   [Trn13Descricao]      VARCHAR(50)    NOT NULL,
   [Trn13Autenticacao]   VARCHAR(10)    NOT NULL,
   [Trn13DataInc]        DATETIME    NOT NULL,
+  [PixConId]            DECIMAL(18)    NULL,
      PRIMARY KEY ( [Trn13Id] ))
+CREATE NONCLUSTERED INDEX [ITRN17] ON [Trn13] (
+      [PixConId])
+ALTER TABLE [Trn13]
+ ADD CONSTRAINT [ITRN17] FOREIGN KEY ( [PixConId] ) REFERENCES [PixCon]([PixConId])
+
+ ALTER TABLE [Trn12]
+ADD [PixConId] DECIMAL(18)    NULL
+CREATE NONCLUSTERED INDEX [ITRN18] ON [Trn12] (
+      [PixConId])
+ALTER TABLE [Trn12]
+ ADD CONSTRAINT [ITRN18] FOREIGN KEY ( [PixConId] ) REFERENCES [PixCon]([PixConId])
