729fa45f1613276c83879d0d31ab07d7531ab308
thilipe <felipe.podipa@gmail.com>
2023-05-03 16:50:21 -0300

    view0002

--- Scripts/Ver_QA/25_#21735_AgendaRecebimento_Adquirente_Felipe_26042023.sql
+++ Scripts/Ver_QA/25_#21735_AgendaRecebimento_Adquirente_Felipe_26042023.sql
@@ -22,7 +22,7 @@ ALTER VIEW [dbo].[AgdRec02] as
 		AgdRc2VlrAju = sum( case when isnull(MovTrnCod,'')='AJ'then VlrVlrRec Else 0 end ),
 		AgdRc2VlrCan = sum( case when isnull(MovTrnCod,'')='CC'then VlrVlrRec Else 0 end ),
 		AgdRc2VlrOut = sum( case when isnull(MovTrnCod,'')=''  then VlrVlrRec Else 0 end ),
-		AgdPc2VlrPix = sum( case when m.MovTrnTipPrd ='P'  then VlrVlrRec Else 0 end)
+		AgdRc2VlrPix = sum( case when m.MovTrnTipPrd ='P'  then VlrVlrRec Else 0 end)
 from vlrrec v left join movtrn01 m on (m.movtrnid =  v.VlrMovTrnId ) where v.VlrStsRec not in (4,5) 
 	group by v.VlrDtaPrvLiqAnt , v.estcod;
 		
@@ -36,7 +36,7 @@ CREATE view [dbo].[AgdRec03] as
 		AgdRc3VlrAju 	= sum( case when isnull(MovTrnCod,'')='AJ'then VlrVlrRec Else 0 end ),
 		AgdRc3VlrCan 	= sum( case when isnull(MovTrnCod,'')='CC'then VlrVlrRec Else 0 end ),
 		AgdRc3VlrOut 	= sum( case when isnull(MovTrnCod,'')=''  then VlrVlrRec Else 0 end ),
-		AgdPc3VlrPix 	= sum( case when m.MovTrnTipPrd ='P'  then VlrVlrRec Else 0 end)
+		AgdRc3VlrPix 	= sum( case when m.MovTrnTipPrd ='P'  then VlrVlrRec Else 0 end)
 from vlrrec v inner join movtrn01 m on (m.movtrnid =  v.VlrMovTrnId ) 
 inner join ADQ0001 a
 on (a.AdqCod = m.AdqCod)
