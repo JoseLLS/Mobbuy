daca3310d9143e1f2afe9722638b1ed0058a1504
JoseLLS <jose.luiz.lemos19@gmail.com>
2023-05-22 08:57:16 -0300

    feat: alteração em script

--- Scripts/Ver_QA/22_#21996_ParametroSaldoConsolidaEC_Jose_18052023.sql
+++ Scripts/Ver_QA/22_#21996_ParametroSaldoConsolidaEC_Jose_18052023.sql
@@ -1,4 +1,6 @@
 /* TAREFA #21996 */
 
+USE Pronto
+
 INSERT INTO PARSIS
 VALUES ('SALDOCONSOLIDA_EC', 'Intervalo de estabelecimentos para o robô SaldoConsolida, padrão parâmetro: EstCod-EstCod', 'VA', 3, NULL, 'N', '1-1', 'ADM', '2023-05-18 09:18:00.000', NULL, NULL, 0)
