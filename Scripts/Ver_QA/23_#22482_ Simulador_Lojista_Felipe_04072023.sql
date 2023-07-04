981d4d851a4dfa2ad639edeb3f3b35f1fba05ffc
thilipe <felipe.podipa@gmail.com>
2023-07-04 17:28:33 -0300

    simulador lojista

---
+++ Scripts/Ver_QA/23_#22482_ Simulador_Lojista_Felipe_04072023.sql
@@ -0,0 +1,3 @@
+INSERT INTO Pronto.dbo.PARSIS
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParQtdDec, ParIndSin, ParCon, ParUsuInc, ParDtiInc, ParUsuAlt, ParDtiAlt, ParFlgInt)
+VALUES(N'HABILITAR_RETORNO', N'Habilitar retorno simulador lojista', N'CA', null, NULL, N'N', N'S', NULL, NULL, NULL, '2023-07-04 10:00:00.000', 0);
