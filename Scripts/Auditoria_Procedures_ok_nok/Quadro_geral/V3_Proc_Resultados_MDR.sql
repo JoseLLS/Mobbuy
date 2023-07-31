1ea74c0f3e8eb81b284edb5d0ba67fc0448566a5
lvsantosdev <ldsantos.developer@gmail.com>
2023-07-31 10:42:38 -0300

    novas procedures quadro geral

---
+++ Scripts/Auditoria_Procedures_ok_nok/Quadro_geral/V3_Proc_Resultados_MDR.sql
@@ -0,0 +1,317 @@
+USE [Pronto]
+GO
+
+/****** Object:  StoredProcedure [dbo].[ConcResultadosLoop]    Script Date: 26/07/2023 11:34:03 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER PROCEDURE [dbo].[ConcResultadosLoop](@DATAINI DATE, @DATAFIM DATE)
+AS
+BEGIN
+
+CREATE TABLE #_DataTemporaria 
+(
+	DataTemp Date
+)
+
+DECLARE @DataTemp DATE;
+DECLARE @ITERADOR INT;
+DECLARE @I INT;
+
+SET @DataTemp = @DATAINI
+
+SET @ITERADOR = DATEDIFF(DAY, @DATAINI, @DATAFIM);
+SET @I = 0;
+
+WHILE @I <= @ITERADOR
+BEGIN
+	
+	INSERT INTO #_DataTemporaria VALUES (@DataTemp)
+	SET @DataTemp = DATEADD(DAY, 1, @DataTemp)
+	SET @I = @I + 1;
+
+END
+
+CREATE TABLE #_ConcResultadosVendas 
+(
+	ConcResultadosData Date,
+	ConcResultadosVendas VARCHAR(5)
+)
+
+CREATE TABLE #_ConcResultadosAntecip 
+(	
+	ConcResultadosData Date,
+	ConcResultadosAntecip VARCHAR(5)
+)
+
+CREATE TABLE #_ConcResultadosCessao 
+(	
+	ConcResultadosData Date,
+	ConcResultadosCessao VARCHAR(5)
+)
+
+CREATE TABLE #_ConcResultadosMDR 
+(	
+	ConcResultadosData Date,
+	ConcResultadosMDR VARCHAR(5)
+)
+
+DELETE FROM ConcResultados
+
+INSERT INTO #_ConcResultadosVendas
+(
+	ConcResultadosData,
+	ConcResultadosVendas
+)
+
+SELECT DT.DataTemp,
+CASE
+	WHEN (MOB.ValorMob = ADQ.ValorAdq AND Mob.ValorMob = Sitef.ValorSitef)  THEN 'OK'
+	WHEN (MOB.ValorMob IS NULL AND ADQ.ValorAdq IS NULL AND Sitef.ValorSitef IS NULL)  THEN 'OK'
+	ELSE 'NOK'
+END
+FROM
+#_DataTemporaria DT
+LEFT JOIN
+(
+	SELECT COALESCE(SUM(MovTrnVlr),0) 'ValorMob', MovTrnDta 
+	FROM MovTrn01
+	WHERE MovTrnDta BETWEEN @DATAINI AND @DATAFIM
+	AND MovTrnCod IN ('CV', 'CC', 'PS')
+	AND MovTrnTipPrd <> 'P'
+	GROUP BY MovTrnDta
+) Mob
+ON DT.DataTemp = Mob.MovTrnDta
+LEFT JOIN 
+(
+	SELECT COALESCE(SUM(ValorTrn),0) 'ValorSitef', VanWbsDat FROM (
+		SELECT  
+		IIF(VanWbsDsc LIKE '%CANCELAMENTO%', ((CONVERT(NUMERIC(17,2),VanWbsVlr))/100) * -1, ((CONVERT(NUMERIC(17,2),VanWbsVlr))/100)) 'ValorTrn', VanWbsDat
+		FROM VAN02 
+		WHERE VanWbsDat BETWEEN @DATAINI AND @DATAFIM
+		AND VanErrCod = 0
+		AND VanWbsSta LIKE '%CONFIRMADA%') A GROUP BY VanWbsDat
+) Sitef
+ON Mob.MovTrnDta = Sitef.VanWbsDat
+LEFT JOIN 
+(
+	SELECT A.TrnBrsOprOriDat, COALESCE(A.ValorTRN05, 0) + COALESCE(B.ValorTrnPagSeg, 0) AS 'ValorAdq' FROM (
+	
+		SELECT COALESCE(SUM(Valor),0) 'ValorTRN05', TrnBrsOprOriDat FROM (
+			SELECT
+				   TrnBrsOprOriDat, 
+				   CASE 
+						WHEN TrnBrsOprBan = '002' THEN 'M'
+						WHEN TrnBrsOprBan = '003' THEN 'V'
+						WHEN TrnBrsOprBan = '004' THEN 'E'
+						WHEN TrnBrsOprBan = '010' THEN 'C'
+						WHEN TrnBrsOprBan = '006' THEN 'D'
+						ELSE ''
+				   END AS Bandeira,
+				   SUBSTRING(Trnbrsoprideser, 4, 1) Produto,
+				   Trnbrsoprmovnsucrr NSU,
+				   IIF(TrnBrsRdeSit < 20, Trnbrsoprvlr, Trnbrsoprvlr *-1) Valor, 
+				   trnbrsoprtrm as PosNroSer,
+				   concat (trnbrsoprbin,trnbrsoprpan )  as ConTrnNumCar    
+			FROM trn05    
+			WHERE TrnBrsRdeSit in ('00','01','03','04')
+			AND TrnBrsOprOriDat BETWEEN @DATAINI AND @DATAFIM
+			group by TrnBrsOprOriDat, Trnbrsoprideser, Trnbrsoprmovnsucrr, 
+			trnbrsoprtrm, trnbrsoprbin, trnbrsoprpan, TrnBrsOprBan, Trnbrsoprvlr, TrnBrsRdeSit) TRN05
+			GROUP BY TrnBrsOprOriDat) A
+	LEFT JOIN (
+	
+		SELECT COALESCE(SUM(Valor),0) 'ValorTrnPagSeg', DataTrn FROM (
+			SELECT 
+				   convert(datetime, trnpagsegdatainicial, 121) 'DataTrn',
+				   CASE
+					WHEN TrnPagSegInstituicao = 'AMEX' THEN 'A'
+					WHEN TrnPagSegInstituicao =	'CABAL' THEN 'C'
+					WHEN TrnPagSegInstituicao =	'ELO' THEN 'E'
+					WHEN TrnPagSegInstituicao =	'HIPERCARD' THEN 'H'
+					WHEN TrnPagSegInstituicao =	'MAESTRO' THEN 'M'
+					WHEN TrnPagSegInstituicao =	'MASTERCARD' THEN 'M'
+					WHEN TrnPagSegInstituicao =	'VISA' THEN 'V'
+					WHEN TrnPagSegInstituicao =	'VISA ELECTRON' THEN 'V'
+					ELSE ''
+					END AS ConTrnBan,
+				   iif(TrnPagSegMeioPagamento = 3, 'C', 'D') ConTrnTipPrd,
+				   trnpagsegcvcodigo ConTrnNsu,
+				   IIF(TrnPagSegTipoEvento = 1, trnpagsegvalororiginal, trnpagsegvalororiginal *-1) Valor,  
+				   concat (trnpagsegcartaobin,trnpagsegcartaoholder )  as ConTrnNumCar,
+				   TrnPagSegTipoEvento
+			FROM TrnPagSeg
+			WHERE convert(datetime, trnpagsegdatainicial, 121) BETWEEN @DATAINI AND @DATAFIM
+			group by trnpagsegdatainicial, TrnPagSegMeioPagamento, trnpagsegcvcodigo, 
+			trnpagsegvalororiginal, trnpagsegcartaobin, trnpagsegcartaoholder, TrnPagSegInstituicao, TrnPagSegTipoEvento) TRNPAGSEG
+			GROUP BY DataTrn) B 
+	ON A.TrnBrsOprOriDat = B.DataTrn
+) Adq
+ON Mob.MovTrnDta = Adq.TrnBrsOprOriDat
+
+INSERT INTO #_ConcResultadosAntecip 
+(
+	ConcResultadosData,
+	ConcResultadosAntecip
+)
+
+SELECT DT.DataTemp,
+COALESCE(CASE
+	WHEN (A.Valor = B.Valor)  THEN 'OK'
+	WHEN A.Valor IS NULL AND B.Valor IS NULL THEN 'OK'
+	ELSE 'NOK'
+END, 'OK')
+FROM 
+#_DataTemporaria DT
+LEFT JOIN 
+(
+SELECT CONVERT(DATE, AnpDtiApv) 'DataAprov', SUM(B.PaaVlrPagAnt) 'Valor' 
+FROM ANTPAG A
+LEFT JOIN PAGANT B
+ON A.AnpNumAnt = B.AnpNumAnt
+WHERE CONVERT(DATE, AnpDtiApv) BETWEEN @DATAINI AND @DATAFIM
+AND ANPSTSANT = 2
+GROUP BY CONVERT(DATE, AnpDtiApv)) A
+ON DT.DataTemp = A.DataAprov
+LEFT JOIN (
+SELECT CONVERT(DATE, AnpDtiApv) 'DataAprov', SUM(C.VlpVlrPag) 'Valor' 
+FROM ANTPAG A
+LEFT JOIN LANANT B
+ON A.AnpNumAnt = B.AnpNumAnt
+LEFT JOIN VLRPAG C
+ON B.LaaNumLan = C.VlpNumLan
+LEFT JOIN ARQDET D
+ON C.VlpArbNum = D.ArbNum AND C.VlpArbLotNum = D.ArbLotNum AND C.VlpArbDetSeq = D.ArbDetSeq
+WHERE CONVERT(DATE, AnpDtiApv) BETWEEN @DATAINI AND @DATAFIM
+AND A.ANPSTSANT = 2
+AND C.VlpStspag = 2
+AND D.ArbDetCodSit IN (0, 4)
+GROUP BY CONVERT(DATE, AnpDtiApv)) B
+ON A.DataAprov = B.DataAprov
+
+INSERT INTO #_ConcResultadosCessao
+SELECT DT.DataTemp,
+COALESCE(CASE
+	WHEN (A.ValorCessaoMobPed = B.ValorCessaoAPIPed) AND (C.ValorCessaoMobPago = D.ValorCessaoAPIPago) THEN 'OK'
+	WHEN (A.ValorCessaoMobPed IS NULL AND B.ValorCessaoAPIPed IS NULL AND C.ValorCessaoMobPago IS NULL AND D.ValorCessaoAPIPago IS NULL) THEN 'OK'
+ELSE 'NOK'
+END, 'OK')
+FROM #_DataTemporaria DT
+LEFT JOIN (
+	SELECT
+	CONVERT(DATE, CessaoDtalIQ) 'DataMobPed',
+	COALESCE(SUM(CessaoValor), 0) 'ValorCessaoMobPed'	
+	FROM Cessao
+	WHERE CONVERT(DATE, CessaoDtalIQ) BETWEEN @DATAINI AND @DATAFIM 
+	AND CessaoStatusId = 1
+	GROUP BY CONVERT(DATE, CessaoDtalIQ)
+) A
+ON DT.DataTemp = A.DataMobPed
+LEFT JOIN (
+	SELECT
+	CONVERT(DATE, CessaoDtalIQ) 'DataAPIPed',
+	COALESCE(SUM(CessaoValor), 0) 'ValorCessaoAPIPed'	
+	FROM Cessao
+	WHERE CONVERT(DATE, CessaoDtalIQ) BETWEEN @DATAINI AND @DATAFIM 
+	AND CessaoStatusId = 1
+	AND CessaoNFDistribuidora <> ''
+	GROUP BY CONVERT(DATE, CessaoDtalIQ)
+) B
+ON DT.DataTemp = B.DataAPIPed
+LEFT JOIN (
+	SELECT
+	CONVERT(DATE, CessaoDtalIQ) 'DataMobPago',
+	COALESCE(SUM(CessaoValor), 0) 'ValorCessaoMobPago'	
+	FROM Cessao
+	WHERE CONVERT(DATE, CessaoDtalIQ) BETWEEN @DATAINI AND @DATAFIM 
+	AND CessaoStatusId = 1
+	GROUP BY CONVERT(DATE, CessaoDtalIQ)
+) C
+ON DT.DataTemp = C.DataMobPago
+LEFT JOIN (
+	SELECT
+	CONVERT(DATE, CessaoDtalIQ) 'DataAPIPago',
+	COALESCE(SUM(CessaoValor), 0) 'ValorCessaoAPIPago'	
+	FROM Cessao
+	WHERE CONVERT(DATE, CessaoDtalIQ) BETWEEN @DATAINI AND @DATAFIM 
+	AND CessaoStatusId = 1
+	AND CessaoNFDistribuidora <> ''
+	GROUP BY CONVERT(DATE, CessaoDtalIQ)
+) D
+ON DT.DataTemp = D.DataAPIPago
+
+INSERT INTO #_ConcResultadosMDR
+SELECT DT.DataTemp,
+CASE 
+	WHEN A.AdqValorRealizado = B.AdqValorPrevisto AND A.AdqValorRealizado = C.ClienteValorRealizado AND A.AdqValorRealizado = D.ClienteValorPrevisto THEN 'OK'
+	WHEN A.AdqValorRealizado IS NULL AND B.AdqValorPrevisto IS NULL AND C.ClienteValorRealizado IS NULL AND D.ClienteValorPrevisto IS NULL THEN 'OK'
+	ELSE 'NOK'
+END
+FROM #_DataTemporaria DT
+LEFT JOIN
+(
+	SELECT COALESCE(SUM(MovTrnGbpVlrTxaInt), 0) 'AdqValorRealizado',
+	MovTrnDta
+	FROM MovTrn01
+	WHERE MovTrnDta BETWEEN @DATAINI AND @DATAFIM
+	AND MovTrnCod in ('CV', 'PS')
+	GROUP BY MovTrnDta) A
+ON DT.DataTemp = A.MovTrnDta
+LEFT JOIN (
+	SELECT COALESCE(SUM(MovTrnTxaComVlrPrv), 0) 'AdqValorPrevisto',
+	MovTrnDta
+	FROM MovTrn01
+	WHERE MovTrnDta BETWEEN @DATAINI AND @DATAFIM
+	AND MovTrnCod in ('CV', 'PS')
+	GROUP BY MovTrnDta
+) B
+ON A.MovTrnDta = B.MovTrnDta
+LEFT JOIN (
+	SELECT COALESCE(SUM(MovTrnBfaVlrTxaAdm), 0) 'ClienteValorRealizado',
+	MovTrnDta
+	FROM MovTrn01
+	WHERE MovTrnDta BETWEEN @DATAINI AND @DATAFIM
+	AND MovTrnCod in ('CV', 'PS')
+	GROUP BY MovTrnDta
+) C
+ON A.MovTrnDta = C.MovTrnDta
+LEFT JOIN (
+	SELECT COALESCE(SUM(MovTrnTxaEstVlrPrv), 0) 'ClienteValorPrevisto',
+	MovTrnDta
+	FROM MovTrn01
+	WHERE MovTrnDta BETWEEN @DATAINI AND @DATAFIM
+	AND MovTrnCod in ('CV', 'PS')
+	GROUP BY MovTrnDta
+) D
+ON A.MovTrnDta = D.MovTrnDta
+
+INSERT INTO ConcResultados 
+(
+	ConcResultadosData,
+	ConcResultadosAgenda,
+	ConcResultadosAntAdq,
+	ConcResultadosAntecip,
+	ConcResultadosCerc,
+	ConcResultadosCessao,
+	ConcResultadosMDR,
+	ConcResultadosPIX,
+	ConcResultadosPrestServ,
+	ConcResultadosVendas
+)
+SELECT A.ConcResultadosData, 'OK', 'OK', B.ConcResultadosAntecip, 'OK', C.ConcResultadosCessao, ConcResultadosMDR, 'OK', 'OK', A.ConcResultadosVendas
+FROM #_ConcResultadosVendas A
+LEFT JOIN #_ConcResultadosAntecip B
+ON A.ConcResultadosData = B.ConcResultadosData
+LEFT JOIN #_ConcResultadosCessao C
+ON A.ConcResultadosData = C.ConcResultadosData
+LEFT JOIN #_ConcResultadosMDR D
+ON A.ConcResultadosData = D.ConcResultadosData
+
+END
+
+GO
