b3086c7c7bcda8d1d61d7f3c02dab94169646f19
JoseLLS <jose.luiz.lemos19@gmail.com>
2023-08-09 16:45:53 -0300

    alteração de scripts

--- Scripts/Ver_QA/23_#22538_Multi-EC_Felipe_07072023.sql
+++ Scripts/Ver_QA/23_#22538_Multi-EC_Felipe_07072023.sql
@@ -5,3 +5,7 @@ ADD [PosNumMultiECPai] INT    NULL,
     DROP INDEX [IPOS5] ON [POS]
 CREATE NONCLUSTERED INDEX [UPOS2] ON [POS] (
       [PosNumSer])
+
+INSERT INTO Pronto.dbo.PARSIS
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParQtdDec, ParIndSin, ParCon, ParUsuInc, ParDtiInc, ParUsuAlt, ParDtiAlt, ParFlgInt)
+VALUES(N'wsmbps_url3', N'Complemento da URL do serviço Multi EC', N'CA', 256, 0, NULL, N'/InterfaceManagerMobbuy/interface/api/estrutura', N'Sistema             ', '2023-08-09 16:16:17.463', NULL, NULL, 0);
