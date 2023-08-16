03c29e704cae813934bd3466f2b7985166f86107
lvsantosdev <ldsantos.developer@gmail.com>
2023-08-16 10:09:30 -0300

    novos campos tabela movtrn

---
+++ Scripts/Ver_QA/25_SemTarefa_novos_campos_movtrn_Leonardo_16082023.sql
@@ -0,0 +1,8 @@
+alter table movtrn01
+add MovTrnTxaComVlrPrv numeric(12,2) null,
+	MovTrnTxaEstVlrPrv NUMERIC(12,2) null,
+	MovTrnEstTxaAdmPrv NUMERIC(6,2) null,
+	MovTrnEstTxaAntPrv NUMERIC(6,2) null,
+	MovTrnEstTarCredPrv NUMERIC(6,2) null,
+	MovTrnEstCusTrnPrv NUMERIC(6,2) null,
+	MovTrnTxaEstCond BIT NULL 
