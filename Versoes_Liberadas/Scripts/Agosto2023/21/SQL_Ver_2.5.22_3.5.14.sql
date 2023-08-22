44753de01b9e1cf405b6e00e6d000c10e67c54a3
JoseLLS <jose.luiz.lemos19@gmail.com>
2023-08-22 14:26:19 -0300

    organização de scripts liberados

---
+++ Versoes_Liberadas/Scripts/Agosto2023/21/SQL_Ver_2.5.22_3.5.14.sql
@@ -0,0 +1,10 @@
+/* SEM TAREFA - JOSÉ */
+
+INSERT INTO SQLHistorico
+VALUES ('3.5.14','2.5.22',GETDATE())
+
+/* TAREFA #22798 - FELIPE */
+
+UPDATE CredPag.dbo.Configuracao
+SET ConfigAplicacao=N'pagmodaev15' 
+WHERE ConfigId=2;
