83a4ecc791f7a2327761a9a251bf6e4051d7f56c
thilipe <felipe.podipa@gmail.com>
2023-08-23 14:55:09 -0300

    ola

---
+++ Scripts/Ver_QA/26_#22814 Parametrizar_APItransacaoGateway_Felipe_23082023.sql
@@ -0,0 +1,3 @@
+INSERT INTO Pronto.dbo.PARSIS
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParQtdDec, ParIndSin, ParCon, ParUsuInc, ParDtiInc, ParUsuAlt, ParDtiAlt, ParFlgInt)
+VALUES(N'Link_Relatorio_Manager', N'Link da API Operação Gateway', N'VA', 100, NULL, N'N', N'https://apigateway-qa.mobbuyapp.com/Consultas/Operacoesgateway', N'ADMIN', '2023-08-23 00:00:00.000', NULL, NULL, 0);
