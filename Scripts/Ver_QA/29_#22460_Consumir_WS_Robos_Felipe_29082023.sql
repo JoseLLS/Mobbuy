d4ac337b549a162921cff5cfa402444759853e57
thilipe <felipe.podipa@gmail.com>
2023-08-29 15:07:04 -0300

    ola

---
+++ Scripts/Ver_QA/29_#22460_Consumir_WS_Robos_Felipe_29082023.sql
@@ -0,0 +1,11 @@
+INSERT INTO CredPag.dbo.PARSIS
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParQtdDec, ParIndSin, ParCon, ParUsuInc, ParDtiInc, ParUsuAlt, ParDtiAlt, ParFlgInt)
+VALUES(N'API_JOB', N'Endereço para executar jobs', N'', 255, NULL, N'N', N'172.16.4.6', NULL, NULL, NULL, NULL, 0);
+
+INSERT INTO CredPag.dbo.PARSIS
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParQtdDec, ParIndSin, ParCon, ParUsuInc, ParDtiInc, ParUsuAlt, ParDtiAlt, ParFlgInt)
+VALUES(N'API_JOB_BASEURL', N'Base URL do Endereço para executar jobs', N'', 255, NULL, N'N', N'/api/21/job/', NULL, NULL, NULL, NULL, 0);
+
+INSERT INTO CredPag.dbo.PARSIS
+(ParCod, ParDsc, ParTipPar, ParTamPar, ParQtdDec, ParIndSin, ParCon, ParUsuInc, ParDtiInc, ParUsuAlt, ParDtiAlt, ParFlgInt)
+VALUES(N'API_JOB_TOKEN', N'Token para executar jobs', N'  ', 255, NULL, N'N', N'l4rE1XSykeaKvVcfa86qYEgoDHWNp0V1', NULL, NULL, NULL, NULL, 0);
