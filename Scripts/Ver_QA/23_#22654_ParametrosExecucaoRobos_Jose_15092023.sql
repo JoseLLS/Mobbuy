9c0131803cd1d640cb482e0cf8af14b2c7da7118
JoseLLS <jose.luiz.lemos19@gmail.com>
2023-09-15 10:27:23 -0300

    Novo script

---
+++ Scripts/Ver_QA/23_#22654_ParametrosExecucaoRobos_Jose_15092023.sql
@@ -0,0 +1,13 @@
+/* TAREFA #22654 - JOSÉ */
+
+/* Banco Monitor */
+
+INSERT INTO PARAMETRO
+VALUES ('SUDO_ATIVADO', 'Verifica se o sudo está habilitado no ambiente ou não para rodar robôs', 'N');
+
+INSERT INTO PARAMETRO
+VALUES ('CAMINHO_ROBOS', 'Caminho no servidor onde se encontra os robôs do sistema', 
+'/usr/local/tomcat/webapps/pronto/WEB-INF/classes/');
+
+INSERT INTO PARAMETRO
+VALUES ('DIR_LOG_TMP', 'LOG TEMPORARIO', '/mnt/home1/sistema/logs/');
