3828627a0d1c3a17f9c66cf6ef5d1ff0758085b0
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2024-01-09 10:29:04 -0300

    Update 31_#23647_INSERT_MENU_wpEstabSemMov_Carlos_04012024.sql

--- Scripts/Ver_QA/31_#23647_INSERT_MENU_wpEstabSemMov_Carlos_04012024.sql
+++ Scripts/Ver_QA/31_#23647_INSERT_MENU_wpEstabSemMov_Carlos_04012024.sql
@@ -68,3 +68,17 @@ VALUES (26, 'wpEstabSemMov_Grid')
 INSERT INTO sse2_grp_mod
 VALUES (26, 'CADASTROS', 'wpEstabSemMov_Grid')
 /*********************************************************************************************************************/
+USE ESTBANK
+
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('wpEstabSemMov_Grid','wpEstabSemMov_Grid', 'Estabelecimentos sem Movimentação', '', '', 0, 1, '')
+
+INSERT INTO sse2_mnu02 (MnuCod, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'wpEstabSemMov_Grid', 'Estabelecimentos sem Movimentação', 125, 'CRED_ESTAB', '', '/estbank/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (28, 'wpEstabSemMov_Grid')
+
+INSERT INTO sse2_grp_mod
+VALUES (28, 'CADASTROS', 'wpEstabSemMov_Grid')
+/*********************************************************************************************************************/
