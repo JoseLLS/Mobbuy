7c747c9b12a29c35f4b24481219e15a4e0ab9e54
lvsantosdev <ldsantos.developer@gmail.com>
2024-01-15 13:36:47 -0300

    sh e script link de pagamento pronto

---
+++ Scripts/Ver_QA/28_#sem_tarefa_parametros_link_pronto_Leonardo_15012024.sql
@@ -0,0 +1,5 @@
+use pronto
+go
+
+insert into PARSIS (ParCod, ParCon) values ('Link_Relatorio_Manager2', 'https://apicomolatti-prd.mobbuyapp.com/Consultas/Operacoesgateway')
+insert into PARSIS (ParCod, ParCon) values ('MANAGERAPI_HOST2', 'https://apicomolatti-prd.mobbuyapp.com')
