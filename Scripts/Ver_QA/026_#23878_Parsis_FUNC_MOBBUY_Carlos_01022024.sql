55c53b1940bbe99f7c24e42c0ee14f15a3813048
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2024-02-01 16:08:33 -0300

    Novo ParSis

---
+++ Scripts/Ver_QA/026_#23878_Parsis_FUNC_MOBBUY_Carlos_01022024.sql
@@ -0,0 +1,20 @@
+--use Banese
+--use Credinov
+--use Credpag
+--use Estbank
+--use pronto
+--use Smartpagamentos
+
+INSERT INTO PARSIS VALUES(
+'FUNC_MOBBUY',
+'Funcionalidades exclusivas para funcionários ligados à Mobbuy',
+'CA',
+1,
+NULL,
+'N',
+'S',
+'025CARLOSP',
+'2024-02-01',
+NULL,
+NULL,
+0)
