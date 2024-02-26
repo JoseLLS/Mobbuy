6317510a2b193b0bf5d6d8a862a0ff23f03ba95d
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-02-26 14:18:55 -0300

    novo script

---
+++ Scripts/Ver_QA/29_#23982_ParametrosComparacaoVendasPOSSistema_Jose_26022024.sql
@@ -0,0 +1,15 @@
+/* TAREFA #23982 -  JOSÉ */
+
+INSERT INTO PARSIS
+VALUES ('MANAGER_TRN_CAPTURA','Url da API de transações da captura','VA',100,NULL,'N','/InterfaceManagerMobbuy/interface/api/transacoes/captura','ADMIN',GETDATE(),NULL,NULL,0);
+
+INSERT INTO PARSIS
+VALUES ('MANAGER_URL','Url do manager','VA',100,NULL,'N','www.mobbuyapp.com:10443','ADMIN',GETDATE(),NULL,NULL,0);
+
+INSERT INTO PARSIS
+VALUES ('COMPARA_POS_EMAIL','E-mails para envio de alerta de bloqueio de transações (separados por ";").','VA',200,NULL,'N',
+'compliance@semprepronto.com.br;financeiro@semprepronto.com.br','ADMIN',GETDATE(),NULL,NULL,0);
+
+INSERT INTO PARSIS
+VALUES ('COMPARA_POS_EMAIL_MSG','Corpo do e-mail para envio de alerta de bloqueio de transações.','VA',200,NULL,'N',
+'<p>Realizamos o bloqueio das transações do EC [EC] e POS [POS] devido a divergência de informações com o Manager.<br>Favor verificar.</p>','ADMIN',GETDATE(),NULL,NULL,0);
