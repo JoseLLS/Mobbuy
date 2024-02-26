1d72276103fe4c7f4755dfd7b0df57c6733455c8
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-02-26 14:43:11 -0300

    novo script

---
+++ Scripts/Ver_QA/30_#23991_ParametroVerificaSaldo_Jose_26022024.sql
@@ -0,0 +1,4 @@
+/* TAREFA #23991 -  JOSÉ */
+
+INSERT INTO PARSIS
+VALUES ('CANCELAMENTO_SALDO','verrifica ou não o saldo para cancelamento de transação.','VA',1,NULL,'N','S','ADMIN',GETDATE(),NULL,NULL,0);
