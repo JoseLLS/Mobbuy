938318d51a341cb50df5e949332a66aae9dd22e1
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2024-02-28 16:06:13 -0300

    Criação de tabela

---
+++ Scripts/Versoes_com_GX17/Fevereiro2024/28/15_#24038_Smartpagamentos_CREATE TABLE RtEstorno_Carlos_28022024.sql
@@ -0,0 +1,16 @@
+USE SmartPagamentos
+CREATE TABLE [RtEstorno] (
+  [RtEstornoId]          DECIMAL(18)    NOT NULL    IDENTITY ( 1 , 1 ),
+  [RtEstornoEstCod]      INT    NULL,
+  [RtEstornoNsu]         DECIMAL(18)    NULL,
+  [RtEstornoAutCod]      VARCHAR(10)    NULL,
+  [RtEstornoDataTrn]     DATETIME    NULL,
+  [RtEstornoProduto]     VARCHAR(1)    NULL,
+  [RtEstornoBandeira]    VARCHAR(1)    NULL,
+  [RtEstornoTrnCod]      VARCHAR(10)    NULL,
+  [RtEstornoValorTrn]    DECIMAL(17,2)    NULL,
+  [RtEstornoValorLiqEst] DECIMAL(17,2)    NULL,
+  [RtEstornoValor]       DECIMAL(17,2)    NULL,
+  [RtEstornoDataInc]     DATETIME    NULL,
+  [RtEstornoGuid]        VARCHAR(100)    NULL,
+     PRIMARY KEY ( [RtEstornoId] ))
