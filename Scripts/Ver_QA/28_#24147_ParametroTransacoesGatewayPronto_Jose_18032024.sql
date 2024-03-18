35ff9d3a20b38189a1808d631757f254b20e9939
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-03-18 11:08:52 -0300

    novo script

---
+++ Scripts/Ver_QA/28_#24147_ParametroTransacoesGatewayPronto_Jose_18032024.sql
@@ -0,0 +1,4 @@
+/* TAREFA #24147 - JOSÉ */
+
+INSERT INTO PARSIS
+VALUES ('OPR_GTW_PRONTO_URL','URL de operações gateway da Pronto','VA',100,NULL,'N','https://apicomolatti-prd.mobbuyapp.com/Consultas/Operacoesgateway','025MJOSE',GETDATE(),NULL,NULL,0)
