109445f84870378be9d3198cb79507223bc21437
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-08-19 17:26:16 -0300

    organização de script

---
+++ Scripts/Ver_QA/32_#24803_ParametroURLParceiro_Jose_19082024.sql
@@ -0,0 +1,4 @@
+/* TAREFA #24803 - JOSÉ */
+
+INSERT INTO PARSIS
+VALUES ('URL_PARCEIRO','URL do sistema Lojista Parceiro','VA',100,NULL,'N','https://permite.per.com.br/permiteparcom','ADMIN',GETDATE(),NULL,NULL,0)
