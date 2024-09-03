5dd726d238ef990055ada0049a9f83c2aefe0400
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-09-03 14:27:27 -0300

    organização de scripts

---
+++ Versoes_Liberadas/Scripts/Setembro2024/03/Loj_2.5.46.sql
@@ -0,0 +1,9 @@
+/* VERSIONAMENTO DE SCRIPTS */
+
+INSERT INTO SQLHistorico
+VALUES ('3.7.5','2.5.46',GETDATE());
+
+/* TAREFA #24803 - JOSÉ */
+
+INSERT INTO PARSIS
+VALUES ('URL_PARCEIRO','URL do sistema Lojista Parceiro','VA',100,NULL,'N','https://permite.per.com.br/permiteparcom','ADMIN',GETDATE(),NULL,NULL,0)
