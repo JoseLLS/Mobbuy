3d2cf6c1225025411e3927793a025097b5e9b49f
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-10-11 16:34:50 -0300

    novo script

---
+++ Scripts/Ver_QA/42_#25249_ParametroArquivoExportacaoSLC_Jose_11102024.sql
@@ -0,0 +1,4 @@
+/* TAREFA #25249 - JOSÉ */
+
+INSERT INTO PARSIS
+VALUES ('ARQ_SLC_EXP','Caminho de exportação do arquivo SLC','VA',300,NULL,'N','/mnt/home/subadquirência/025/reembolso/SLC/env/','ADMIN',GETDATE(),NULL,NULL,0)
