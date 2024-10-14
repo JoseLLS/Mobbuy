31827a6137c08382aa671edea123d5019d391fbd
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-10-14 15:07:27 -0300

    alteração de script

--- Scripts/Ver_QA/42_#25249_ParametroArquivoExportacaoSLC_Jose_11102024.sql
+++ Scripts/Ver_QA/42_#25249_ParametroArquivoExportacaoSLC_Jose_11102024.sql
@@ -1,4 +1,8 @@
 /* TAREFA #25249 - JOSÉ */
 
 INSERT INTO PARSIS
-VALUES ('ARQ_SLC_EXP','Caminho de exportação do arquivo SLC','VA',300,NULL,'N','/mnt/home/subadquirência/025/reembolso/SLC/env/','ADMIN',GETDATE(),NULL,NULL,0)
+VALUES ('ARQ_SLC_EXP','Caminho de exportação do arquivo SLC','VA',300,NULL,'N','/mnt/home/subadquirência/025/reembolso/SLC/env/','ADMIN',GETDATE(),NULL,NULL,0);
+
+ALTER TABLE [TblBan]
+ADD [TblBanSlcCodArranjo] SMALLINT    NULL,
+    [TblBanTipo] CHAR(2)    NULL;
