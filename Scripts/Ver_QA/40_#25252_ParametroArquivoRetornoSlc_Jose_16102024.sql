5a4595baca2bf7c2f357ecb243421a326a673eca
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-10-16 16:57:33 -0300

    novo script

---
+++ Scripts/Ver_QA/40_#25252_ParametroArquivoRetornoSlc_Jose_16102024.sql
@@ -0,0 +1,2 @@
+INSERT INTO PARSIS
+VALUES ('ARQ_SLC_RET','Caminho de exportação do arquivo SLC','VA',300,NULL,'N','/mnt/home1/SubAdquirencia/025/reembolso/SLC/retornoprevia/','ADMIN',GETDATE(),NULL,NULL,0);
