48eb9731f9b8246255dbd4b4ba39728c548e8c4f
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-10-23 10:44:34 -0300

    alteração de script

--- Scripts/Ver_QA/41_#25097_NovaTelaTipoDocumento_Ruan_21102024.sql
+++ Scripts/Ver_QA/41_#25097_NovaTelaTipoDocumento_Ruan_21102024.sql
@@ -1,15 +1,13 @@
 /* TAREFA #25097 - RUAN */
 
 INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
-VALUES ('trTipoDocumentosGrid', 'trTipoDocumentosGrid', 'Lista de tipos de documentos', '', '', 0, 1, '');
+VALUES ('trTipoDocumentoGrid', 'trTipoDocumentoGrid', 'Lista de tipos de documentos', '', '', 0, 1, '');
 
 INSERT INTO sse2_mnu02 (MnuCod, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
-VALUES (10, 1, 'trTipoDocumentosGrid', 'Lista de tipos de documentos', 11, 'TAB_GER', '', '/pronto/servlet/')
-
-SELECT * FROM sse2_mnu02 WHERE MnuIteIdRoot = 'TAB_GER' ORDER BY MnuIteOrd
+VALUES (10, 1, 'trTipoDocumentoGrid', 'Lista de tipos de documentos', 11, 'TAB_GER', '', '/permite/servlet/')
 
 INSERT INTO sse2_ung_mod (ung2Cod, mod2Id)
-VALUES (25, 'trTipoDocumentosGrid')
+VALUES (30, 'trTipoDocumentoGrid')
 
 INSERT INTO sse2_grp_mod
-VALUES (25, 'ADM', 'trTipoDocumentosGrid') -- Sempre será ADM para Lista de tipos de documentos
+VALUES (30, 'ADM', 'trTipoDocumentoGrid')
