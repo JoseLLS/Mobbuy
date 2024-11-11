0495dec20580a844669681b8f99eab85f2e1b40a
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-11-11 16:03:21 -0300

    retaguarda nova versão

---
+++ Scripts/Ver_QA/43_#25459_ParametrosAPIBaneseRedeCaptura_Jose_11112024.sql
@@ -0,0 +1,13 @@
+/* TAREFA #??? - JOSÉ */
+
+INSERT INTO PARSIS
+VALUES ('BANESE_REDECAP','Link da API de Rede Captura da Banese','VA',300,NULL,'N','https://api-priv.mulvi.com.br/pppay/contabil/v1/Transacao/SubAdiquirencia/Aprovadas','ADMIN',GETDATE(),NULL,NULL,0);
+
+INSERT INTO PARSIS
+VALUES ('BANESE_TOKEN','Link da API que gera o token da Banese','VA',300,NULL,'N','https://api-priv.mulvi.com.br/pppay/contabil/v1/Token/GerarTokenAsync','ADMIN',GETDATE(),NULL,NULL,0);
+
+INSERT INTO PARSIS
+VALUES ('BANESE_TOKEN_USR','Usuário da API que gera o token da Banese','VA',300,NULL,'N','0000052','ADMIN',GETDATE(),NULL,NULL,0);
+
+INSERT INTO PARSIS
+VALUES ('BANESE_TOKEN_SEN','Senha da API que gera o token da Banese','VA',300,NULL,'N','8k5o5mxRMPgR','ADMIN',GETDATE(),NULL,NULL,0);
