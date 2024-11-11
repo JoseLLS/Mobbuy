30cbfb4a3f00c1894f515bd86cef6a4b01a605ef
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-11-11 17:07:17 -0300

    organização de scripts liberados

---
+++ Versoes_Liberadas/Scripts/Novembro2024/11/Ret_3.7.16_Loj_2.5.51_LojPac_2.1.16.sql
@@ -0,0 +1,18 @@
+/* Versionamento de scripts */
+
+INSERT INTO SQLHistorico
+VALUES ('3.7.16','2.5.51',GETDATE());
+
+/* TAREFA #25097 - RUAN */
+
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('trTipoDocumentoGrid', 'trTipoDocumentoGrid', 'Lista de tipos de documentos', '', '', 0, 1, '');
+
+INSERT INTO sse2_mnu02 (MnuCod, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'trTipoDocumentoGrid', 'Lista de tipos de documentos', 11, 'TAB_GER', '', '/permite/servlet/')
+
+INSERT INTO sse2_ung_mod (ung2Cod, mod2Id)
+VALUES (30, 'trTipoDocumentoGrid')
+
+INSERT INTO sse2_grp_mod
+VALUES (30, 'ADM', 'trTipoDocumentoGrid')
