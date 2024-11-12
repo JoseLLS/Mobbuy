2529007e4548b6d10d725bae0154e4cb5d29b4c9
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-11-12 15:53:49 -0300

    novo script

---
+++ Scripts/Ver_QA/43_#25472_NovaTelaBandeiras_Jose_12112024.sql
@@ -0,0 +1,13 @@
+/* TAREFA #25472 - JOSÉ */
+
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('TBLBAN_GRID', 'TBLBAN_GRID', 'Bandeiras', '', '', 0, 1, '');
+
+INSERT INTO sse2_mnu02 (MnuCod, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'TBLBAN_GRID', 'Bandeiras', 232, 'TAB_GER', '', '/pronto/servlet/');
+
+INSERT INTO sse2_ung_mod (ung2Cod, mod2Id)
+VALUES (25, 'TBLBAN_GRID');
+
+INSERT INTO sse2_grp_mod
+VALUES (25, 'ADM', 'TBLBAN_GRID');
