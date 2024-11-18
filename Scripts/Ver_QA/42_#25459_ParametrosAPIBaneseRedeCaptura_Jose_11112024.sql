1eb23ded26afd6ac6f7a92637a362fef54ab6859
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-11-18 09:19:12 -0300

    alteração de script

--- Scripts/Ver_QA/42_#25459_ParametrosAPIBaneseRedeCaptura_Jose_11112024.sql
+++ Scripts/Ver_QA/42_#25459_ParametrosAPIBaneseRedeCaptura_Jose_11112024.sql
@@ -1,4 +1,4 @@
-/* TAREFA #??? - JOSÉ */
+/* TAREFA #25459 - JOSÉ */
 
 INSERT INTO PARSIS
 VALUES ('BANESE_REDECAP','Link da API de Rede Captura da Banese','VA',300,NULL,'N','https://api-priv.mulvi.com.br/pppay/contabil/v1/Transacao/SubAdiquirencia/Aprovadas','ADMIN',GETDATE(),NULL,NULL,0);
@@ -11,3 +11,9 @@ VALUES ('BANESE_TOKEN_USR','Usuário da API que gera o token da Banese','VA',300,
 
 INSERT INTO PARSIS
 VALUES ('BANESE_TOKEN_SEN','Senha da API que gera o token da Banese','VA',300,NULL,'N','8k5o5mxRMPgR','ADMIN',GETDATE(),NULL,NULL,0);
+
+INSERT INTO VAN01
+VALUES(7, 'API Banese', '1753-01-01 00:00:00.000');
+
+ALTER TABLE [VAN04]
+ADD [VanTrnNumPedInt] VARCHAR(50)    NULL;
