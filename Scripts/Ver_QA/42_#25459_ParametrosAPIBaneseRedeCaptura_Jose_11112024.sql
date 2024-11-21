6b77c77976961a3bd564823c9fdc33fc34b8f5c4
JoseLLS <jose.luiz.lemos19@gmail.com>
2024-11-21 12:03:21 -0300

    alteração de script

--- Scripts/Ver_QA/42_#25459_ParametrosAPIBaneseRedeCaptura_Jose_11112024.sql
+++ Scripts/Ver_QA/42_#25459_ParametrosAPIBaneseRedeCaptura_Jose_11112024.sql
@@ -17,3 +17,16 @@ VALUES(7, 'API Banese', '1753-01-01 00:00:00.000');
 
 ALTER TABLE [VAN04]
 ADD [VanTrnNumPedInt] VARCHAR(50)    NULL;
+
+--Script de tela nova
+INSERT INTO sse2_mod (MOD2Id, MOD2IdInt, MOD2Dsc, MOD2Url, MOD2MtdLib, MOD2Aux, MOD2Sts, Mod2LstSub)
+VALUES ('GatewayConvenios','GatewayConvenios', 'Gateway Convênios', '', '', 0, 1, '');
+
+INSERT INTO sse2_mnu02 (MnuCod, MnuIteModFlg, MnuIteIde, MnuIteDsc, MnuIteOrd, MnuIteIdRoot, MnuIteTxt, MnuItePth)
+VALUES (10, 1, 'GatewayConvenios', 'Gateway Convênios', 301, 'CTR_FIN', '', '/banese/servlet/');
+
+INSERT INTO sse2_ung_mod (ung2cod, mod2id)
+VALUES (12, 'GatewayConvenios');
+
+INSERT INTO sse2_grp_mod
+VALUES (12, 'ADM', 'GatewayConvenios');
