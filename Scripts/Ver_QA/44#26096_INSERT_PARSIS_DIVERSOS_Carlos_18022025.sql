a8d2cee221d2a58fc8f405213b780201bd3b517a
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2025-02-18 09:50:39 -0300

    INSERTS DIVERSOS

---
+++ Scripts/Ver_QA/44#26096_INSERT_PARSIS_DIVERSOS_Carlos_18022025.sql
@@ -0,0 +1,11 @@
+INSERT INTO PARSIS VALUES
+
+('APIGATEWAYRECORRENCIA_BASEURL','base url api gateway recorrencia','CA',100,'','N','/Recorrencia/','024ADM','2025-02-18 09:23:43.000','','',0),
+('APIGATEWAYRECORRENCIA_HOST','host api gateway recorrencia','CA',100,'','N','apigateway-qa.mobbuyapp.com','024ADM','2025-02-18 09:22:58.000','','',0),
+('APIGATEWAY_TOKEN_PASSWORD','password gerar token link pagamento recorrencia','CA',100,'','N','1q2w3e4r','024ADM','2025-02-18 09:19:53.000','','',0),
+('APIGATEWAY_TOKEN_USER','usuario gerar token link pagamento','CA',100,'','N','credapi','024ADM','2025-02-18 09:19:08.000','','',0),
+('APIGATEWAY_TOKEN_BASEURL','base url gerar token link pagamento','CA',100,'','N','/Account/','024ADM','2025-02-18 09:18:05.000','','',0),
+('APIGATEWAY_TOKEN_HOST','host gerar token link pagamento recorrencia','CA',100,'','N','apigateway-qa.mobbuyapp.com','024ADM','2025-02-18 09:16:47.000','','',0),
+('LINKPGTORECAPI_URL','url link pagamentos','CA',100,'','N','GerarPlanoViaLink','024ADM','2025-02-18 09:15:51.000','','',0),
+('LINKPGTORECAPI_BASEURL','base url link pagamento recorrência','CA',100,'','N','/Recorrencia/','024ADM','2025-02-18 09:14:51.000','','',0),
+('LINKPGTORECAPI_HOST','host link pagamentos recorrencia','CA',100,'','N','apigateway-qa.mobbuyapp.com','024ADM','2025-02-18 09:10:02.000','','',0)
