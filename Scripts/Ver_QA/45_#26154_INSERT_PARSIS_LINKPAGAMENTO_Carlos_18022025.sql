9f2bc63c6753b83e7f3a0bb8fda0239ebb9a87bb
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2025-02-18 11:15:52 -0300

    insert parsis

---
+++ Scripts/Ver_QA/45_#26154_INSERT_PARSIS_LINKPAGAMENTO_Carlos_18022025.sql
@@ -0,0 +1,6 @@
+use credpag
+
+INSERT INTO PARSIS VALUES
+('LINKPAGAMENTOAPI_HOST','host link pagamentos','CA',100,'','N','apigateway-qa.mobbuyapp.com','024ADM','2025-02-18 09:10:02.000','','',0),
+('LINKPAGAMENTOAPI_URL1','url link pagamentos','CA',100,'','N','Cobranca','024ADM','2025-02-18 09:15:51.000','','',0),
+('LINKPAGAMENTOAPI_URL2','url link pagamentos','CA',100,'','N','Autorizar','024ADM','2025-02-18 09:15:51.000','','',0)
