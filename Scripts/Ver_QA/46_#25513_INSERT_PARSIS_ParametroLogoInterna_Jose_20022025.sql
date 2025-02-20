83f45fec3ffdcc140f6c6078590b1a0b2ea5c318
JoseLLS <jose.luiz.lemos19@gmail.com>
2025-02-20 10:38:34 -0300

    novo script

---
+++ Scripts/Ver_QA/46_#25513_INSERT_PARSIS_ParametroLogoInterna_Jose_20022025.sql
@@ -0,0 +1,4 @@
+/* TAREFA #25513 - JOSÉ */
+
+INSERT INTO PARSIS
+VALUES('LOGO_INTERNA','Caminho da logo interna do sistema Lojista','VA',200,NULL,'N','Resources/LogoIconeCREDPAG.png','ADMIN',GETDATE(),NULL,NULL,0);
