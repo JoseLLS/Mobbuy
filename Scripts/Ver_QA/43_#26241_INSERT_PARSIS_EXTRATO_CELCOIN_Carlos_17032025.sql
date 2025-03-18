8de346346b9ce594f9344a26f7eabdde1ad946b1
cpsjunior <95862714+cpsjunior@users.noreply.github.com>
2025-03-18 11:55:56 -0300

    INSERT PARSIS

---
+++ Scripts/Ver_QA/43_#26241_INSERT_PARSIS_EXTRATO_CELCOIN_Carlos_17032025.sql
@@ -0,0 +1,9 @@
+use --[TODOS]
+
+insert into PARSIS values
+('EXTRATO_CELCOIN_URL','URL que retorna dados da api Extrato Celcoin','CA',255,null,'N','https://www.mobbuyapp.com:10443/InterfaceManagerMobbuy/interface/processos/celcoin/getExtrato?','024ADM','2025-03-17',null,null,0),
+('EXTRATO_CELCOIN_BASIC_USER','User utilizado na autenticação basic da api Extrato Celcoin','CA',255,null,'N','mobbuyguest','024ADM','2025-03-17',null,null,0),
+('EXTRATO_CELCOIN_BASIC_PASSWORD','Password utilizado na autenticação basic da api Extrato Celcoin','CA',255,null,'N','8f543df1d7571e92db14b5fa067552e6','024ADM','2025-03-17',null,null,0),
+('EXTRATO_CELCOIN_TOKEN_USER','User passado no header endpoint da api Extrato Celcoin','CA',255,null,'N','ca158a3be227ac52f232a1460bd8284b','024ADM','2025-03-17',null,null,0),
+('EXTRATO_CELCOIN_TOKEN_PASSWORD','Password passado no header endpoint da api Extrato Celcoin','CA',255,null,'N','c96e66a9c4fc65d221b0daa19b5c81e81eeee0be','024ADM','2025-03-17',null,null,0)
+
