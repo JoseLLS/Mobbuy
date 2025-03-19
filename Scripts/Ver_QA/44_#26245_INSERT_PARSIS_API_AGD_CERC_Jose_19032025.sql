fe41d18b3a3e624bcbfa9c4850cf7308a505bfcd
JoseLLS <jose.luiz.lemos19@gmail.com>
2025-03-19 10:26:51 -0300

    novo script

---
+++ Scripts/Ver_QA/44_#26245_INSERT_PARSIS_API_AGD_CERC_Jose_19032025.sql
@@ -0,0 +1,4 @@
+/* TAREFA #26245 - JOSÉ */
+
+INSERT INTO PARSIS
+VALUES('API_AGD_CERC','Url da API de conciliação de agendas CERC','VA',100,0,'N','https://ap.cerc.inf.br/conciliacao/agenda','ADMIN',GETDATE(),NULL,NULL,0);
